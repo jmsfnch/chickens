4852c8145266da1fd309dda8380a8db402ba3e50
James Finch <jmsfnch@gmail.com>
2019-12-21 09:55:10 +0000

    Starting new animal classes

---
+++ ChickensCSharp/ChickensCSharp/Animal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChickensCSharp
+{
+    class Animal : Organism
+    {
+        public bool IsAnimal { get; set; }
+        public Animal Mother { get; set; }
+        public Animal Father { get; set; }
+    }
+}
---
+++ ChickensCSharp/ChickensCSharp/Organism.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChickensCSharp
+{
+    class Organism
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        // Default Constructor
+        public Organism()
+        {
+            this.Name = "";
+        }
+    }
+}
